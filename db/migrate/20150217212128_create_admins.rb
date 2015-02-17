b4bc8cc1a86452ff29dc4043defdafd3cdcdba87
Michael Campbell <MCcool322@aol.com>
2015-02-17 16:49:26 -0500

    added test schema for login

--- db/migrate/20150217212128_create_admins.rb
+++ db/migrate/20150217212128_create_admins.rb
@@ -1,6 +1,8 @@
 class CreateAdmins < ActiveRecord::Migration
   def change
     create_table :admins do |t|
+      t.string :user_name,  null: false
+      t.string :password_digest,  null: false
     end
   end
 end
