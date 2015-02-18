16d3b568c0669f5b6bdf742e5eed20d91f299616
Philip Hughes <philip@h4w5.com>
2015-02-18 17:04:59 -0500

    rewrite routes to be a little simpler and use the rails conventions

--- config/routes.rb
+++ config/routes.rb
@@ -1,103 +1,49 @@
 Rails.application.routes.draw do
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'login#login'
+  root to: redirect('/login')
 
-  get '/' => 'login#login'
-  get '/login' => 'login#login'
+  get  '/login' => 'login#login'
+  # called select because it selects if you are an admin or a student
   post '/login' => 'login#select'
 
-  get '/admins' => 'admins#admin'
 
-# admin#:location
-# admin#:class
-# admin#:cohort
-# admin#:student_id
+  # admin pov!
+  namespace :admin do
+    # see students list and individual
+    resources :students, only: [:index, :show]
+    # see cohorts list and individual
+    resources :cohorts, only: [:index, :show] do
+      # see students BY cohort as a list
+      # resources :students, only: [:index]
 
-  # admins opt-in page for student search -or- admins listing
-  # get '/admins/index' => 'admins#index'
-  # destroy '/admins/index' => 'admins#destroy'
-  # patch '/admins/index' => 'admins#update'
+      member do
+        get :chart
+      end
+    end
+    resources :courses, only: [:index, :show] do
+      # see cohorts BY course as a list
+      # resources :cohorts, only: [:index]
+    end
+    resources :locations, only: [:index, :show] do
+      # see cohorts BY location as a list
+      # resources :cohorts, only: [:index]
+    end
+  end
 
-  # get '/admins/new' => 'admins#new'
-  # post '/admins/new' => 'admins#create'
+  # student pov!
+  resources :students, only: [:show, :edit, :update] do
+    # the survey form
+    collection do
+      get :survey
+    end
 
-  get '/admins/search' => 'admins#search'
-  # index of locations
-  get '/admins/locations' => 'admins#locations'
-  # show of a location
-  get '/admins/:location' => 'admins#location'
-
-
-
-
-  # get '/admins/search/cohorts' => 'cohorts#index'
-  # get '/admins/search/cohorts/:cohort_id' => 'cohorts#show'
-
-  # get '/admins/cohorts/:cohort_id/:student_id' => 'students#show'
-
-  get '/students' => 'students#index'
-  # get '/students/new' => 'students#create'
-  get '/students/:student_id' => 'students#show'
-  get '/students/:student_id/edit' => 'students#edit'
-  post '/students/:student_id' => 'students#show'
-  get '/students/:student_id/survey' => 'students#survey'
-
-  resources :students
-
-
-
-
-
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+    # where we post the form information
+    member do
+      post :survey
+    end
+  end
 end
