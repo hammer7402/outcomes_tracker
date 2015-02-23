9ffed33df991e0b6479990c0abd41cb52ec71249
Kamari Aykes <aykeskc@gmail.com>
2015-02-23 00:28:07 -0500

    checks schema

--- db/schema.rb
+++ db/schema.rb
@@ -44,19 +44,12 @@ ActiveRecord::Schema.define(version: 20150220165507) do
   end
 
   create_table "students", force: :cascade do |t|
-    t.string  "first",                                 null: false
-    t.string  "last",                                  null: false
-    t.string  "email",                                 null: false
-    t.string  "phone",                                 null: false
-    t.string  "password_digest",                       null: false
-    t.boolean "is_employed",           default: false
-    t.text    "skills"
-    t.string  "employment_type"
     t.string  "first",                                         null: false
     t.string  "last",                                          null: false
     t.string  "email",                                         null: false
     t.string  "phone",                                         null: false
     t.string  "password_digest",                               null: false
+    t.boolean "is_employed",           default: false
     t.text    "skills",                default: "none listed"
     t.string  "employment_type",       default: "unemployed"
     t.date    "employment_start_date"
